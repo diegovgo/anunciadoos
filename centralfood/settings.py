9f25276bddfae3379d57cac2356e49cfdefc854b
diego <dvilela@sacatucita.com>
2021-02-19 01:57:18 -0500

    rest design

--- centralfood/settings.py
+++ centralfood/settings.py
@@ -74,8 +74,8 @@ WSGI_APPLICATION = 'centralfood.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 '''
+
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
